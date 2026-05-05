09a7579265da9cd98510183c2f51ba24800fc381
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-05-04 21:37:20 -0700

    ♻️ Extract render function to main.js
    Move renderTask function into a separate main.js file to
    separate presentation logic from fetching logic.

---
+++ public/main.js
@@ -0,0 +1,16 @@
+function renderTask(task) {
+    return `
+        <table>
+            <tbody>
+                <tr><td>ID</td><td>${task.id}</td></tr>
+                <tr><td>Description</td><td>${task.description}</td></tr>
+                <tr><td>Context</td><td>${task.context}</td></tr>
+                <tr><td>Skip Count</td><td>${task.skip_count}</td></tr>
+                <tr><td>Starred</td><td>${task.starred ? 'Yes' : 'No'}</td></tr>
+                <tr><td>Latest Date</td><td>${task.latest_date}</td></tr>
+                <tr><td>Recurrence</td><td>${task.recurrence_days} days</td></tr>
+                <tr><td>Due Date</td><td>${task.due_date}</td></tr>
+            </tbody>
+        </table>
+    `;
+}
